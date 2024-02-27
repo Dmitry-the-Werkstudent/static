f4dfb70301a80b29114a89771cdb8e1f5e5a07f7
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-02-27 17:10:07 +0100

    Update conditional.js

--- js/conditional.js
+++ js/conditional.js
@@ -1,27 +1,44 @@
-function conditional(cond) {
-  cond = {
-    "Reiseorganisation": {
-      "Ich bitte um Reiseorganisation": {
-        "Wie planen Sie anzureisen?": {
-          "Flug": "Abflug-Flughafen",
-          "Bahn": {
-            "Abfahrt-Bahnhof": null,
-            "Ich habe eine BahnCard": {
-              "Ja, {...}": ["BahnCard Nummer", "BahnCard Ablaufdatum"]
-            }
+function conditional(cond, show, dict) {
+  show = show ?? true;
+  dict = dict ?? { };
+
+  if (cond.constructor === "".constructor) {
+    // single question without further action
+    if (show) {
+      $(".customization2_attendee_further-data_custom-question:contains('" + cond + "')").parent().show();
+    } else {
+      $(".customization2_attendee_further-data_custom-question:contains('" + cond + "')").parent().hide();
+    }
+  
+  } else if (cond.constructor === [].constructor) {
+    // questions without further action
+    cond.forEach(c => conditional(c, show, dict));
+
+  } else {
+    for (const question in cond) {
+      const answers = cond[question] ?? { };
+      const el = $(".customization2_attendee_further-data_custom-question:contains('" + question + "')");
+
+      if (!(question in dict)) {
+        function handle() {
+          const selected = el.find("input:checked").next(".customization2_attendee_further-data_custom-question_radio-line_label")
+            .find(".vv-radio__label-text").text().trim();
+          for (const possible in answers) {
+            conditional(answers[possible], possible == selected, dict);
           }
+        }
+        el.on("change", handle);
+        dict[question] = handle;
+      }
+      dict[question]();
+      if (show) {
+        el.parent().show();
+      } else {
+        el.parent().hide();
+        for (const answer in answers) {
+          conditional(answers[answer], false, dict);
         }
       }
     }
-  };
-
-  if (cond.constructor === [].constructor) {
-    
   }
-  
-  for (const question in cond) {
-    const answers = cond[question] ?? { };
-    
-  }
-  
 }

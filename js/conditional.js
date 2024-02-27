f1bed3632c9498a6bd0c126e84d9142fc85161cb
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-02-27 18:37:18 +0100

    Update conditional.js

--- js/conditional.js
+++ js/conditional.js
@@ -17,29 +17,31 @@ function conditional(cond, show, dict) {
       const answers = cond[q] ?? { };
       const el = findQuestion(question);
 
+      // setup handler for sub-answers
+      if (!(question in dict)) {
+        function handle() {
+          let selected;
+          if (type == "*") selected = el.find("input:checked").next(".customization2_attendee_further-data_custom-question_radio-line_label").find(".vv-radio__label-text").text().trim();
+          if (type == "|") selected = el.find(".customization2_attendee_further-data_custom-question_dropdown .vv-selection-input__value").text().trim();
+
+          for (const possible in answers) {
+            const match = (
+              possible == selected ||
+              (possible.startsWith("{...}") && selected.endsWith(possible.slice(5))) ||
+              (possible.endsWith("{...}") && selected.startsWith(possible.slice(0, -5)))
+            );
+            conditional(answers[possible], match, dict);
+          }
+        }
+        // setup change listener
+        el.on("change", handle);
+        dict[question] = handle;
+      }
+      dict[question]();
+
       // hide/show the actual question
       if (show) {
         el.parent().show();
-        // setup handler for sub-answers
-        if (!(question in dict)) {
-          function handle() {
-            let selected;
-            if (type == "*") selected = el.find("input:checked").next(".customization2_attendee_further-data_custom-question_radio-line_label").find(".vv-radio__label-text").text().trim();
-            if (type == "|") selected = el.find(".customization2_attendee_further-data_custom-question_dropdown .vv-selection-input__value").text().trim();
-            for (const possible in answers) {
-              const match = (
-                possible == selected ||
-                (possible.startsWith("{...}") && selected.endsWith(possible.slice(5))) ||
-                (possible.endsWith("{...}") && selected.startsWith(possible.slice(0, -5)))
-              );
-              conditional(answers[possible], match, dict);
-            }
-          }
-          // setup change listener
-          el.on("change", handle);
-          dict[question] = handle;
-        }
-        dict[question]();
       } else {
         el.parent().hide();
         // hide all sub-questions

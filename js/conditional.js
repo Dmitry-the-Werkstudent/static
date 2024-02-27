d22a185ea1fc70b89a1b9eb602d171105e001a0a
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-02-27 18:29:09 +0100

    Update conditional.js

--- js/conditional.js
+++ js/conditional.js
@@ -1,4 +1,4 @@
-function conditional(cond, show, dict) {
+function conditional(cond, show, set) {
   if (cond.constructor === "".constructor) {
     // single question without further action
     const el = findQuestion(cond);
@@ -8,42 +8,42 @@ function conditional(cond, show, dict) {
       el.parent().hide();
     }
   } else if (cond.constructor === [].constructor) {
-    // questions without further action
-    cond.forEach(c => conditional(c, show, dict));
+    // multiple questions without further action
+    cond.forEach(c => conditional(c, show, set));
   } else {
     for (const q in cond) {
       const type = q[0];
       const question = q.slice(1);
       const answers = cond[q] ?? { };
       const el = findQuestion(question);
-      console.log(q, el);
 
-      if (!(question in dict)) {
-        function handle() {
-          let selected;
-          if (type == "*") selected = el.find("input:checked").next(".customization2_attendee_further-data_custom-question_radio-line_label").find(".vv-radio__label-text").text().trim();
-          if (type == "|") selected = el.find(".customization2_attendee_further-data_custom-question_dropdown .vv-selection-input__value").text().trim();
-          for (const possible in answers) {
-            const match = (
-              possible == selected ||
-              (possible.startsWith("{...}") && selected.endsWith(possible.slice(5))) ||
-              (possible.endsWith("{...}") && selected.startsWith(possible.slice(0, -5)))
-            );
-            run(answers[possible], match, dict);
-          }
-        }
-
-        el.on("change", handle);
-        dict[question] = handle;
-      }
-      dict[question]();
+      // hide/show the actual question
       if (show) {
         el.parent().show();
+        // setup handler for sub-answers
+        if (!set.has(question)) {
+          function handle() {
+            let selected;
+            if (type == "*") selected = el.find("input:checked").next(".customization2_attendee_further-data_custom-question_radio-line_label").find(".vv-radio__label-text").text().trim();
+            if (type == "|") selected = el.find(".customization2_attendee_further-data_custom-question_dropdown .vv-selection-input__value").text().trim();
+            for (const possible in answers) {
+              const match = (
+                possible == selected ||
+                (possible.startsWith("{...}") && selected.endsWith(possible.slice(5))) ||
+                (possible.endsWith("{...}") && selected.startsWith(possible.slice(0, -5)))
+              );
+              conditional(answers[possible], match, set);
+            }
+          }
+          // setup change listener
+          el.on("change", handle);
+          set.add(question);
+          handle();
+        }
       } else {
         el.parent().hide();
-        for (const answer in answers) {
-          conditional(answers[answer], false, dict);
-        }
+        // hide all sub-questions
+        for (const answer in answers) { conditional(answers[answer], false, set); }
       }
     }
   }
@@ -56,5 +56,5 @@ function findQuestion(exactLabel) {
 }
 
 function run(cond) {
-  conditional(cond, true, { });
+  conditional(cond, true, new Set());
 }

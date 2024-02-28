b95ba32caf50d472a9e8e5f05c529f370d25ea64
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-02-28 15:42:10 +0100

    Update custom-js-loader.js

--- js/custom-js-loader.js
+++ js/custom-js-loader.js
@@ -1,5 +1,7 @@
-window.custom_js = function (name, ...args) {
-  $.getScript("https://dmitry-the-werkstudent.github.io/static/js/" + name + ".js", () => {
-    run(...args);
+window.custom_js = async function (name, ...args) {
+  return new Promise(resolve => {
+    $.getScript("https://dmitry-the-werkstudent.github.io/static/js/" + name + ".js", async () => {
+      resolve(await run(...args));
+    });
   });
 }

ad05c84cd7e7914b7d9516ca83319b98df621c2c
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-02-28 15:50:58 +0100

    Update custom-js-loader.js

--- js/custom-js-loader.js
+++ js/custom-js-loader.js
@@ -1,6 +1,12 @@
+const cache = { };
+
 window.custom_js = async function (name, ...args) {
+  if (name in cache) {
+    return await cache[name](...args);
+  }
   return new Promise(resolve => {
     $.getScript("https://dmitry-the-werkstudent.github.io/static/js/" + name + ".js", async () => {
+      cache[name] = run;
       resolve(await run(...args));
     });
   });

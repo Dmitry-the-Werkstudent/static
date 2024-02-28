c112c651c0ad4d96e161e29cba03e2d77c56a1f5
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-02-28 16:31:18 +0100

    Update findQuestion.js

--- js/findQuestion.js
+++ js/findQuestion.js
@@ -2,8 +2,16 @@ async function run(exactLabel) {
   return $(".customization2_attendee_further-data_custom-question").filter((i, q) => {
     const label = $(q).find(".customization2_attendee_further-data_custom-question_label");
     const allText = [...label.get(0).childNodes].map(c => $(c).text().trim()).join("");
-    let match = exactLabel;
-    if(label.find("vv-optional-text").length) match += label.find("vv-optional-text").text().trim();
-    return allText == match;
+    if (exactLabel.endsWith("{...}")) {
+      return allText.startsWith(exactLabel.slice(0, -5));
+    } else if (exactLabel.startsWith("{...}")) {
+      let match = exactLabel.slice(5);
+      if(label.find("vv-optional-text").length) match += label.find("vv-optional-text").text().trim();
+      return allText.endsWith(match);
+    } else {
+      let match = exactLabel;
+      if(label.find("vv-optional-text").length) match += label.find("vv-optional-text").text().trim();
+      return allText == match;
+    }
   });
 }

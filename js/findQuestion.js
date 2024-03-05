aafea1ef19973b4376ca3780e186f81929f6d76e
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-03-05 13:33:46 +0100

    Update findQuestion.js

--- js/findQuestion.js
+++ js/findQuestion.js
@@ -2,17 +2,19 @@ async function run(exactLabel, dom) {
   const q = $(".customization2_attendee_further-data_custom-question").filter((i, q) => {
     const label = $(q).find(".customization2_attendee_further-data_custom-question_label");
     const allText = [...label.get(0).childNodes].map(c => $(c).text().trim()).join("");
-    if (exactLabel.endsWith("{...}")) {
-      return allText.startsWith(exactLabel.slice(0, -5));
-    } else if (exactLabel.startsWith("{...}")) {
-      let match = exactLabel.slice(5);
-      if(label.find("vv-optional-text").length) match += label.find("vv-optional-text").text().trim();
-      return allText.endsWith(match);
-    } else {
-      let match = exactLabel;
-      if(label.find("vv-optional-text").length) match += label.find("vv-optional-text").text().trim();
-      return allText == match;
+    let match = exactLabel;
+    if(label.find("vv-optional-text").length) match += label.find("vv-optional-text").text().trim();
+
+    const groups = match.split("{...}");
+    let i = 0;
+    for (const group of groups) {
+      const sub = allText.slice(i);
+      const idx = sub.indexOf(group);
+      if (idx == -1) return false;
+      i += idx + group.length;
     }
+    return true;
+
   }).parent();
   if (dom) return q.get(0);
   else return q;
